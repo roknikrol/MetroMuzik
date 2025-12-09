4d9e8656b1978c5bb93d093d324f280beda0c89e
roknikrol <53019360+roknikrol@users.noreply.github.com>
2025-12-08 20:06:38 -0500

    Add MetroMuzika iOS target and initial files
    Introduces a new MetroMuzika iOS target to the Xcode project, including app, unit test, and UI test targets. Adds initial SwiftUI app and view files, asset catalogs, and test stubs for the new iOS target. Also updates project configuration and schemes to support the new targets.

---
+++ MetroMuzika iOS/MetroMuzika_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroMuzika_iOSApp.swift
+//  MetroMuzika iOS
+//
+//  Created by Nikita Podobedov on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MetroMuzika_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MetroMuzika iOSTests/MetroMuzika_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  MetroMuzika_iOSTests.swift
+//  MetroMuzika iOSTests
+//
+//  Created by Nikita Podobedov on 12/8/25.
+//
+
+import Testing
+@testable import MetroMuzika_iOS
+
+struct MetroMuzika_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
